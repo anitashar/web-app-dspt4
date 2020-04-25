00c775fb93823586710c25c65ce597f22b3c7d2e
anitashar <anitashar2006@gmail.com>
2020-04-24 21:54:43 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # web-app-dspt4
+
+Installation
+TODO: instructions for git clone
+
+Setup
+TODO: instructions for virtual environment
+
+To install virtual environment
+Pipenv install
+
+To activate virtual env
+
+Pipenv shell
+
+To install flask
+pipenv install Flask Flask-SQLAlchemy Flask-migrate
+
+set up database:
+
+    FLASK_APP=web_app flask db init #> generates app/migrations dir
+    # run both when changing the schema:
+    FLASK_APP=web_app flask db migrate #> creates the db (with "alembic_version" table)
+    FLASK_APP=web_app flask db upgrade #> creates the specified tables
+Usage
+# Mac:
+FLASK_APP=hello.py flask run
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -8,12 +8,24 @@
 # web_app/__init__.py
 
 from flask import Flask
-
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
+
+DATABASE_URI = "sqlite://///Users/anitasharma/lambdaschool/DS-Unit-3-Sprint-3-Productization-and-Cloud/web-app-dspt4/twitoff_development.db"
+SECRET_KEY = "super secret"
+
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY #It will enable flash messaging via sessions
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI 
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     return app
@@ -21,3 +33,5 @@ def create_app():
 if __name__ == "__main__":
     my_app = create_app()
     my_app.run(debug=True)
+
+    
