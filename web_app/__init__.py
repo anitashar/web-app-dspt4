7b763e013b41797b69d4ef3db4f3b334aceaeedc
anitashar <anitashar2006@gmail.com>
2020-05-02 14:29:12 -0700

    updated for database

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,13 +1,9 @@
-
-
-
-
-
-   
-
 # web_app/__init__.py
 
+import os
+from dotenv import load_dotenv
 from flask import Flask
+
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
@@ -15,29 +11,28 @@ from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.admin_routes import admin_routes
 from web_app.routes.stats_routes import stats_routes
 
+load_dotenv()
 
-DATABASE_URI = "sqlite://///Users/anitasharma/lambdaschool/DS-Unit-3-Sprint-3-Productization-and-Cloud/web-app-dspt4/twitoff_development.db"
-SECRET_KEY = "super secret"
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = SECRET_KEY #It will enable flash messaging via sessions
+    app.config["SECRET_KEY"] = SECRET_KEY # enable flash messaging via sessions
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI 
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
-
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     app.register_blueprint(twitter_routes)
     app.register_blueprint(admin_routes)
     app.register_blueprint(stats_routes)
+
     return app
 
 if __name__ == "__main__":
     my_app = create_app()
     my_app.run(debug=True)
-
-    
